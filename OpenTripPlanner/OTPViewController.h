fb628ffc184b589c32cb5afa33cc19aafa26605d
Aaron Sutula <asutula@openplans.org>
2012-08-30 13:16:09 -0400

    Added storyboard and removed MapKit based files for now.

---
+++ OpenTripPlanner/OTPViewController.h
@@ -0,0 +1,13 @@
+//
+//  OTPViewController.h
+//  OpenTripPlanner
+//
+//  Created by asutula on 8/30/12.
+//  Copyright (c) 2012 OpenPlans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTPViewController : UIViewController
+
+@end
---
+++ OpenTripPlanner/OTPViewController.m
@@ -0,0 +1,43 @@
+//
+//  OTPViewController.m
+//  OpenTripPlanner
+//
+//  Created by asutula on 8/30/12.
+//  Copyright (c) 2012 OpenPlans. All rights reserved.
+//
+
+#import "OTPViewController.h"
+
+@interface OTPViewController ()
+
+@end
+
+@implementation OTPViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
