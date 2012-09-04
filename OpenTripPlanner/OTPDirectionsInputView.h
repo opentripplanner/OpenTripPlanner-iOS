a1b30b4f6d45de8bf34b9818ba04765febb4e155
Aaron Sutula <asutula@openplans.org>
2012-09-04 15:06:36 -0400

    Undoing some view work

---
+++ OpenTripPlanner/OTPDirectionsInputView.h
@@ -0,0 +1,13 @@
+//
+//  OTPDirectionsInputView.h
+//  OpenTripPlanner
+//
+//  Created by asutula on 9/1/12.
+//  Copyright (c) 2012 OpenPlans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OTPDirectionsInputView : UIToolbar
+
+@end
